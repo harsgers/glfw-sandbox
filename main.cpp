b298231998f06d53ffaccba7e8da7ae985cc7adf
harsgers <harrison.gerstenlauer@gmail.com>
2024-11-09 09:33:16 -0500

    test

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ const char *vertex_shader = "#version 400\n"
                             "  gl_Position = vec4(vp, 1.0);"
                             "}";
 
-const char *fragment_shader = "#version 400\n"
+const char *fragment_shader = "#version 410\n"
                               "out vec4 frag_colour;"
                               "void main() {"
                               "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
